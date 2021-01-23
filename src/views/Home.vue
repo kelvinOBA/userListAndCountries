a1403ef144ca6266e08d7b9d12730d08e14f2ca8
kelvin.igbinoba <kelvin.igbinoba@redwoodscapital.co>
2021-01-23 15:40:42 +0100

    Add google font

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,226 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="parent-cover" >
+
+<div>
+  <p><span class="tix-offblue">tix.</span><span class="tix-main">africa</span></p>
+</div>
+      <div class="user-detail animate__animated animate__fadeInUp" v-if="showUserObject">
+
+          <p class="user-email"><b> {{userObject.name}}</b></p>
+          <p>Country(s)</p>
+          <template v-if="userObject.country.length">
+
+          <p v-for="country in userObject.country" :key="country.country_id"> {{country.country_id}}</p>
+
+
+
+          </template>
+          <p  class="user-email" v-else>Sorry no country for this user</p>
+          <button @click="showUserObject = false" class="guess-btn">Close</button>
+      </div>
+
+  <template v-if="!showUserObject">
+      <div class="user-component animate__animated animate__fadeIn" v-for="data in returnUserData(userData)" :key="data.id"> 
+        <div>
+        <img src="../assets/profile_pic.png" height="60" alt="">
+
+        </div>
+        <div>
+          <h4>{{data.name}}</h4>
+          <p class="user-email">Jamman@sampete.co</p>
+        </div>
+        <div>
+       <button class="guess-btn" @click="checkUser(data)">Guess <i class="fa fa-spinner fa-spin" v-if="userId == data.id"></i> </button>
+        </div>
+      </div>
+  </template>
+    </div>
+ <template v-if="!showUserObject">
+  <a href="#" class="btn-default animate__animated animate__fadeInUp" :class="{'paginate-active': index+1 == currentPage || index == 0 && currentPage == 0 }" v-for="(count, index) in paginate_total" :key="count.index" @click="updateCurrentPage(count)">
+    {{ count }}
+  </a>
+    </template>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import HelloWorld from "@/components/HelloWorld.vue";
+import fakeData from "../../fakeData.json"
+import axios from 'axios'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: "Home",
+  data(){
+return{
+userData:[],
+userId:'',
+startSlice:0,
+endSlice:4,
+paginate_total:[],
+paginate:4,
+currentPage: 0,
+userObject:{
+  name:''
+},
+showUserObject : false
 }
+
+  },
+  components: {
+    HelloWorld,
+  },
+
+  methods: {
+
+    returnUserData(data){
+
+      return data.slice(this.startSlice, this.endSlice)
+    },
+
+    updateCurrentPage(index){
+
+      this.currentPage = index
+      this.setPaginate(index)
+      console.log(this.currentPage)
+    },
+       setPaginate(pageNumber) {
+       if (pageNumber == 1) {
+       this.startSlice = 0 
+       this.endSlice = 4
+       }
+       else {
+       this.startSlice = this.paginate * (pageNumber - 1)
+       this.endSlice = pageNumber *this.paginate
+console.log(this.startSlice)
+       }
+     },
+
+     checkUser(user){
+       this.userId = user.id
+ axios.get(`https://api.nationalize.io/?name=${user.name}`)
+        .then(response => {
+          console.log(response, "response");
+
+        this.userObject = response.data
+        this.showUserObject = true
+        this.userId = ''
+        })
+        .catch(error => {
+          if (error.response) {
+            console.log(error.response, "error");
+           
+          }
+        });
+     }
+  },
+
+  mounted(){
+    
+    this.userData = fakeData
+     this.paginate_total = this.userData.length/this.paginate;
+  }
+};
 </script>
+
+
+<style lang="scss" scoped>
+
+.parent-cover{
+font-family: 'Roboto', sans-serif;
+max-width: 500px;
+margin: 0 auto;
+}
+
+.user-detail{
+  border-radius: 10px;
+        border: 1px solid rgba(0,0,0,0.078);
+          background: #fff;
+          min-height: 200px;
+          padding: 20px;
+}
+.user-component{
+
+  background: #fff;
+  display: flex;
+  margin-bottom:30px;
+  padding: 10px;
+      border:2px solid rgba(0,0,0,0.078);
+    box-shadow: 0px 1px 6px 0px rgba(0,0,0,0.071);
+  border-radius: 10px;
+  transition: .4s;
+    align-items: center;
+  div{
+    flex-basis: 30%;
+  }
+  h4,p{
+    text-align: left;
+    margin: 0; 
+  }
+ .user-email{
+   color: #858796;
+   font-size: 13px;
+     text-align: left;
+ }
+}
+
+.user-component:hover{
+    cursor: pointer;
+        box-shadow: 0px 1px 6px 2px rgba(0,0,0,0.091);
+    border: 2px solid  #0385956b;
+    transition: .4s;
+}
+
+.guess-btn{
+  background: #fc6435;
+  border: #fc6435;
+  color: #fff;
+  border-radius: 3px;
+  padding: 10px;
+  cursor: pointer;
+}
+.btn-default {
+    color: #fff;
+    background-color: #fc6435;
+    border-color: #fc6435;
+        display: inline-block;
+    padding: 6px 12px;
+    margin-bottom: 0;
+    font-size: 14px;
+    font-weight: normal;
+    line-height: 1.42857143;
+    text-align: center;
+    text-decoration: none;
+    margin-right: 2px;
+    margin-bottom:4px;
+    white-space: nowrap;
+    vertical-align: middle;
+    -ms-touch-action: manipulation;
+    touch-action: manipulation;
+    cursor: pointer;
+  
+    user-select: none;
+ 
+    border: 1px solid #f2f2f2;
+    border-radius: 4px;
+}
+.paginate-active{
+  background-color: #fff;
+  color: #fc6435;
+    border-color: #fc6435;
+}
+
+
+.tix-offblue{
+
+
+color:   #038595;
+}
+
+.tix-main{
+
+
+color:   #fc6435;
+}
+</style>
